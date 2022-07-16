155227f36da1c2a3d023e9731abbf76c6ba18739
wongfunian <wongfunian@hotmail.com>
2022-07-16 22:21:41 +0800

    Initiate Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default Home
